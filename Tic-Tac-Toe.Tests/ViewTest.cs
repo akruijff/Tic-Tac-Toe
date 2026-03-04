c390f6431926146a5a9e9c6ab126e6cdc037cf2d
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-03-04 19:18:21 +0100

    Add project files.

---
+++ Tic-Tac-Toe.Tests/ViewTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Tic_Tac_Toe.Tests
+{
+    public class ViewTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            StringWriter writer = new();
+            View view = new(writer);
+            view.draw();
+            string[] expected =
+            {
+            "Tic Tac Toe"
+            ""
+            "+-------+"
+            "| # # # |"
+            "| # # # |"
+            "| # # # |"
+            "+-------+"
+            "";
+            "Your move.";
+
+        }
+    }
+    }
+}
---
+++ Tic-Tac-Toe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tic_Tac_Toe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
