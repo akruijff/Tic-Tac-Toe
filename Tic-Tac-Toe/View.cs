281e92a859e2961239a9e6cbc3fcd0efe0f93b5d
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-03-04 19:36:49 +0100

    Draw the board at startup

---
+++ Tic-Tac-Toe/View.cs
@@ -0,0 +1,22 @@
+﻿namespace Tic_Tac_Toe
+{
+    internal class View
+    {
+        /// <summary>
+        /// Draws the board to the console.
+        /// </summary>
+        public void Draw()
+        {
+            Console.Clear();
+            Console.WriteLine("Welkom to Tic Tac Toe");
+            Console.WriteLine("");
+            Console.WriteLine("+-------+");
+            Console.WriteLine("| # # # |");
+            Console.WriteLine("| # # # |");
+            Console.WriteLine("| # # # |");
+            Console.WriteLine("+-------+");
+            Console.WriteLine("");
+            Console.WriteLine("Your move");
+        }
+    }
+}
